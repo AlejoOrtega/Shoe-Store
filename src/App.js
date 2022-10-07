20f70b51705db9dda2189e7ed64d5dbadf129d8d
Alejandro Ortega <ajrevollo96@uninorte.edu.co>
2022-10-06 23:23:29 -0400

    Columns added

--- src/App.js
+++ src/App.js
@@ -21,102 +21,110 @@ function App() {
     <div className="App">
 
       {/* 4 Craighouse Shoes */}
-
-      <div className='container'>
-        <div className='like-button'>
-          <span className='heart'>&hearts;</span>
-        </div>
-        <img className='show-image' 
-          src={craighouse_side} 
-          onMouseOver={e => (e.currentTarget.src = craighouse_oh)} 
-          onMouseOut={e => (e.currentTarget.src = craighouse_side)}
-          alt='shoes'/>
-        <p className='promotions'><span className='deal'>-30%</span></p>
-        <div className='information'>
-          <p>Hudson London</p>
-          <h2>CRAIGHOUSE LEATHER</h2>
-          <p><span className='deal-price'>190,00$</span> <span className='regular-price'>220,00$</span></p>
+      <div className='col'>
+        <div className='container'>
+          <div className='like-button'>
+            <span className='heart'>&hearts;</span>
+          </div>
+          <img className='show-image' 
+            src={craighouse_side} 
+            onMouseOver={e => (e.currentTarget.src = craighouse_oh)} 
+            onMouseOut={e => (e.currentTarget.src = craighouse_side)}
+            alt='shoes'/>
+          <p className='promotions'><span className='deal'>-30%</span></p>
+          <div className='information'>
+            <p>Hudson London</p>
+            <h2>CRAIGHOUSE LEATHER</h2>
+            <p><span className='deal-price'>190,00$</span> <span className='regular-price'>220,00$</span></p>
+          </div>
         </div>
       </div>
  
 
       {/* 2 Battle Shoes */}
-
-      <div className='container'>
-        <div className='like-button'>
-          <span className='heart'>&hearts;</span>
-        </div>
-        <img className='show-image' 
-          src={battle_side} 
-          onMouseOver={e => (e.currentTarget.src = battle_top)} 
-          onMouseOut={e => (e.currentTarget.src = battle_side)}
-          alt='shoes'/>
-        <p className='promotions'><span className='deal'>-50%</span><span className='eco'>Eco Sostenible</span></p>
-        <div className='information'>
-          <p>Hudson London</p>
-          <h2>BATTLE BROWN</h2>
-          <p><span className='deal-price'>240,00$</span> <span className='regular-price'>220,00$</span></p>
+      <div className='col'>
+        <div className='container'>
+          <div className='like-button'>
+            <span className='heart'>&hearts;</span>
+          </div>
+          <img className='show-image' 
+            src={battle_side} 
+            onMouseOver={e => (e.currentTarget.src = battle_top)} 
+            onMouseOut={e => (e.currentTarget.src = battle_side)}
+            alt='shoes'/>
+          <p className='promotions'><span className='deal'>-50%</span><span className='eco'>Eco Sostenible</span></p>
+          <div className='information'>
+            <p>Hudson London</p>
+            <h2>BATTLE BROWN</h2>
+            <p><span className='deal-price'>240,00$</span> <span className='regular-price'>220,00$</span></p>
+          </div>
         </div>
       </div>
 
       
       {/* 1 Bailey Shoes */}
-
-      <div className='container'>
-        <div className='like-button'>
-          <span className='heart'>&hearts;</span>
-        </div>
-        <img className='show-image' 
-          src={bailey_side} 
-          onMouseOver={e => (e.currentTarget.src = bailey_oh)} 
-          onMouseOut={e => (e.currentTarget.src = bailey_side)}
-          alt='shoes'/>
-        <p className='promotions'><span className='deal'>-25%</span></p>
-        <div className='information'>
-          <p>Hudson London</p>
-          <h2>BALEY</h2>
-          <p><span className='deal-price'>270,00$</span> <span className='regular-price'>300,00$</span></p>
+      <div className='col'>
+        <div className='container'>
+          <div className='like-button'>
+            <span className='heart'>&hearts;</span>
+          </div>
+          <img className='show-image' 
+            src={bailey_side} 
+            onMouseOver={e => (e.currentTarget.src = bailey_oh)} 
+            onMouseOut={e => (e.currentTarget.src = bailey_side)}
+            alt='shoes'/>
+          <p className='promotions'><span className='deal'>-25%</span></p>
+          <div className='information'>
+            <p>Hudson London</p>
+            <h2>BALEY</h2>
+            <p><span className='deal-price'>270,00$</span> <span className='regular-price'>300,00$</span></p>
+          </div>
         </div>
       </div>
   
 
       {/* 3 Cedar Shoes */}
-      <div className='container'>
-        <div className='like-button'>
-          <span className='heart'>&hearts;</span>
-        </div>
-        <img className='show-image' 
-          src={cedar_side} 
-          onMouseOver={e => (e.currentTarget.src = cedar_oh)} 
-          onMouseOut={e => (e.currentTarget.src = cedar_side)}
-          alt='shoes'/>
-        <p className='promotions'><span className='deal'>-50%</span><span className='eco'>Eco Sostenible</span></p>
-        <div className='information'>
-          <p>Hudson London</p>
-          <h2>CEDAR</h2>
-          <p><span className='deal-price'>240,00$</span> <span className='regular-price'>220,00$</span></p>
+      <div className='col'>
+        <div className='container'>
+          <div className='like-button'>
+            <span className='heart'>&hearts;</span>
+          </div>
+          <img className='show-image' 
+            src={cedar_side} 
+            onMouseOver={e => (e.currentTarget.src = cedar_oh)} 
+            onMouseOut={e => (e.currentTarget.src = cedar_side)}
+            alt='shoes'/>
+          <p className='promotions'><span className='deal'>-50%</span><span className='eco'>Eco Sostenible</span></p>
+          <div className='information'>
+            <p>Hudson London</p>
+            <h2>CEDAR</h2>
+            <p><span className='deal-price'>240,00$</span> <span className='regular-price'>220,00$</span></p>
+          </div>
         </div>
       </div>
 
       {/* 6 Marley Shoes */}
-      <div className='container'>
-        <div className='like-button'>
-          <span className='heart'>&hearts;</span>
-        </div>
-        <img className='show-image' 
-          src={marley_side} 
-          onMouseOver={e => (e.currentTarget.src = marley_oh)} 
-          onMouseOut={e => (e.currentTarget.src = marley_side)}
-          alt='shoes'/>
-        <p className='promotions'><span className='deal'>-50%</span></p>
-        <div className='information'>
-          <p>Hudson London</p>
-          <h2>MARLEY</h2>
-          <p><span className='deal-price'>89,00$</span> <span className='regular-price'>110,00$</span></p>
+      <div className='col'>
+        <div className='container'>
+          <div className='like-button'>
+            <span className='heart'>&hearts;</span>
+          </div>
+          <img className='show-image' 
+            src={marley_side} 
+            onMouseOver={e => (e.currentTarget.src = marley_oh)} 
+            onMouseOut={e => (e.currentTarget.src = marley_side)}
+            alt='shoes'/>
+          <p className='promotions'><span className='deal'>-50%</span></p>
+          <div className='information'>
+            <p>Hudson London</p>
+            <h2>MARLEY</h2>
+            <p><span className='deal-price'>89,00$</span> <span className='regular-price'>110,00$</span></p>
+          </div>
         </div>
       </div>
 
       {/* 5 Dylan Shoes */}
+      <div className='col'>
       <div className='container'>
         <div className='like-button'>
           <span className='heart'>&hearts;</span>
@@ -133,6 +141,7 @@ function App() {
           <p><span className='deal-price'>240,00$</span> <span className='regular-price'>220,00$</span></p>
         </div>
       </div>
+    </div>
     </div>
 
   );
