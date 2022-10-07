035dc90e7f070977e59b4d48a49af7cd148175e9
Alejandro Ortega <ajrevollo96@uninorte.edu.co>
2022-10-06 22:16:02 -0400

    first object

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import bailey_side from './img/bailey/bailey_washed_suede_tobacco_side.jpg'
+import bailey_oh from './img/bailey/bailey_washed_suede_tobacco_oh.jpg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='container'>
+        <div className='like-button'>
+          <span className='heart'>&hearts;</span>
+        </div>
+        <img className='show-image' 
+          src={bailey_side} 
+          onMouseOver={e => (e.currentTarget.src = bailey_oh)} 
+          onMouseOut={e => (e.currentTarget.src = bailey_side)}
+          alt='shoes'/>
+        <p className='deal'>-30%</p>
+        <div className='information'>
+          <p>Hudson London</p>
+          <h2>CRAIGHOUSE LEATHER</h2>
+          <p><span className='deal-price'>190,00$</span> <span className='regular-price'>220,00$</span></p>
+        </div>
+      </div>
+
+
+
     </div>
+
   );
 }
 
